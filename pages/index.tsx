cafa981827e2f72ed85bd9251b8d5725dfd7d732
cashev <42924021+cashev@users.noreply.github.com>
2023-09-24 05:19:46 +0000

    show stars of React Repository

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,30 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import { useEffect, useState } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [data, setData] = useState({
+    stars: 0
+  });
+
+  // ここでuseEffectを使ってstar数を取得してみましょう!
+  useEffect(() => {
+    fetch("https://api.github.com/repos/facebook/react")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((json) => {
+        setData({
+          stars: json.stargazers_count
+        });
+      });
+  }, []);
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
@@ -33,6 +54,10 @@ export default function Home() {
         </div>
       </div>
 
+      <div>
+        <p>ここにReactのGitHubレポジトリに付いたスターの数を表示してみよう</p>
+        <p>{data.stars} stars</p>
+      </div>
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
